6d0170881dcd4e31744645b742a5f7a3e31355b8
Daniel Enriquez <cdeh88@gmail.com>
2021-04-08 21:28:35 -0400

    add CRUD methods

--- Server/Controllers/UnitController.cs
+++ Server/Controllers/UnitController.cs
@@ -27,5 +27,52 @@ namespace BlazorBattles.Server.Controllers
             var units = await _context.Units.ToListAsync();
             return Ok(units);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            await _context.Units.AddAsync(unit);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Units.ToListAsync());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            Unit dbUnit = await _context.Units.FirstOrDefaultAsync(unit => unit.Id == id);
+
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with given id doesn't exist.");
+            }
+
+            dbUnit.Title = unit.Title;
+            dbUnit.Attack = unit.Attack;
+            dbUnit.Defense = unit.Defense;
+            dbUnit.HitPoints = unit.HitPoints;
+            dbUnit.BananaCost = unit.BananaCost;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(dbUnit);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            Unit dbUnit = await _context.Units.FirstOrDefaultAsync(unit => unit.Id == id);
+
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with given id doesn't exist.");
+            }
+
+            _context.Units.Remove(dbUnit);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Units.ToListAsync());
+        }
     }
 }
